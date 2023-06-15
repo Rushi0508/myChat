5d183c6c823308e52b9628e4111430e988cd7987
Rushi_0508 <rushigandhi14@gmail.com>
2023-06-15 18:33:27 +0530

    session and router setup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,17 +2,26 @@
 import axios from 'axios'
 import Button from "@/app/components/Button";
 import Input from "@/app/components/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { SubmitHandler,FieldValues, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import {BsGithub, BsGoogle} from 'react-icons/bs'
 import {toast} from 'react-hot-toast'
-import {signIn} from 'next-auth/react'
+import {signIn, useSession} from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     type Variant = 'LOGIN' | 'REGISTER';
     const [variant,setVariant] = useState<Variant>('LOGIN')
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(()=>{
+        if(session?.status === 'authenticated'){
+            router.push('/users');
+        }
+    }, [session?.status, router])
 
     const toggleVariant = useCallback(()=>{
         if(variant==='LOGIN'){
@@ -40,6 +49,7 @@ const AuthForm = () => {
 
         if(variant==='REGISTER'){
             axios.post('/api/register', data)
+            .then(()=>signIn('credentials', data))
             .catch(()=>toast.error('Something went wrong!'))
             .finally(()=>setIsLoading(false))
         }
@@ -53,7 +63,8 @@ const AuthForm = () => {
                     toast.error('Invalid Credentials');
                 }
                 if(callback?.ok && !callback?.error){
-                    toast.success('Logged in!')
+                    toast.success('Logged in!');
+                    router.push('/users');
                 }
             })
             .finally(()=>setIsLoading(false));
---
+++ app/users/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { signOut } from "next-auth/react";
+
+const Users = () => {
+  return ( 
+    <button onClick={()=>signOut()}>Logout</button>
+   );
+}
+
+export default Users
